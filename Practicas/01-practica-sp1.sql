b14795dba0b4764feeb6610c019d74555edf479b
Elliot Tapia Crisostomo <tapiaelliot85@gmail.com>
2024-07-05 13:36:38 -0600

    Manipulacion avanzada : Se agrego un nuevo sp en practica

---
+++ Practicas/01-practica-sp1.sql
@@ -0,0 +1,19 @@
+--Crear un sp que solicite un id de una categoría 
+-- y devuelva el promedio de los precios de sus productos
+
+use NORTHWND
+go
+
+create or alter proc sp_solicitar_promedio_prod
+@catego int --parametro de entrada
+AS
+begin
+select avg(UnitPrice) as 'Promedio precios de los productos'
+from Products
+where CategoryID =  @catego;
+end 
+go
+select * from Products
+
+exec sp_solicitar_promedio_prod 2
+exec sp_solicitar_promedio_prod @catego = 5
